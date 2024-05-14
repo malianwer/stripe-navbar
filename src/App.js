3b995f9a44435c2a51efedb4f0081a849df3d149
Ali <mac@MACs-MacBook-Pro.local>
2024-05-15 01:03:07 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import StripeNavbar from "./components/StripeNavbar/StripeNavbar";
+
+function App() {
+  return (
+    <div style={{ background: "#0b0b12" }}>
+      <StripeNavbar />
+    </div>
+  );
+}
+
+export default App;
